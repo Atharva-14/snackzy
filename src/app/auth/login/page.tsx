cf9f3609ee4ce68285148ddd7600b2754beabe22
Atharva-14 <armdaryapur@gmail.com>
2024-11-19 18:14:53 +0530

    created login page and signup page for supplier

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,70 +1,60 @@
 "use client";
-
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/libs/utils";
 import Link from "next/link";
 
 const Page = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   return (
-    <div className="flex flex-col justify-center items-center bg-stone-100 p-10">
-      <h1 className="font-semibold text-3xl">Welcome Back</h1>
-      <p className="text-neutral-500">
-        Welcome back, please enter your details
-      </p>
+    <div>
+      <div className="max-w-md w-full mx-auto p-4 md:p-8 bg-white ">
+        <h2 className="font-bold text-xl text-neutral-800">Welcome Back</h2>
+        {/* <p className="text-neutral-600 text-sm max-w-sm mt-2 ">
+        Login to snackzy if you can because we don&apos;t have a login flow
+        yet
+      </p> */}
 
-      <form className="flex flex-col justify-center items-center mt-6">
-        <label className="flex items-center gap-2 px-4 py-2 border border-black bg-white rounded-md">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-5 w-5 opacity-90"
+        <form className="my-8" onSubmit={handleSubmit}>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input id="email" placeholder="johndoe@example.com" type="email" />
+          </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" placeholder="••••••••" type="password" />
+          </LabelInputContainer>
+
+          <button
+            className="bg-gradient-to-br relative group/btn from-black   to-neutral-600 block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] (--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
           >
-            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-          </svg>
-          <input
-            className="items-center outline-none"
-            placeholder="johndoe@example.com"
-            type="email"
-            required
-          />
-        </label>
+            Login &rarr;
+            <BottomGradient />
+          </button>
 
-        <label className="flex items-center gap-2 px-4 py-2 border border-black bg-white rounded-md mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-5 w-5 opacity-90"
+          {/* <div className="bg-gradient-to-r from-transparent via-neutral-300  to-transparent my-8 h-[1px] w-full" /> */}
+        </form>
+      </div>
+
+      <div className="flex flex-col items-end">
+        <div className="pr-4 pb-2.5">
+          <p className="text-neutral-600 mb-1 text-center">
+            Grow with Snackzy!
+          </p>
+          <Link
+            href={`/auth/signup/supplier`}
+            className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-200 bg-[linear-gradient(110deg,#edf5ff,45%,#d0e7ff,55%,#edf5ff)]
+ bg-[length:200%_100%] px-6 font-medium text-slate-800 transition-colors focus:outline-none"
           >
-            <path
-              fillRule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <input
-            className="items-center outline-none"
-            placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-            type="password"
-            required
-          />
-        </label>
-
-        <Link
-          href={"#"}
-          className="underline w-full text-end hover:text-blue-600"
-        >
-          Forgot Password?
-        </Link>
-
-        <button
-          className="mt-4 bg-gradient-to-br relative group/btn transform hover:-translate-y-1 transition duration-400 from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
-          Login &rarr;
-          <BottomGradient />
-        </button>
-      </form>
+            Join as a Seller
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
@@ -77,5 +67,19 @@ const BottomGradient = () => {
       <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
       <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
     </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
   );
 };
